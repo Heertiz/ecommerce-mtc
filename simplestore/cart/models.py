4ec334ddf6815928c159f958aa0f2f1e021d71e0
Rian <69772016+Heertiz@users.noreply.github.com>
2026-06-18 23:42:02 -0300

    Issue 21

--- simplestore/cart/models.py
+++ simplestore/cart/models.py
@@ -6,7 +6,7 @@ from simplestore.products.models.product import Product
 
 class Cart(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-    session_key = models.CharField(max_length=255, null=True)
+    session_key = models.CharField(max_length=255, blank=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     active = models.BooleanField(default=True)
